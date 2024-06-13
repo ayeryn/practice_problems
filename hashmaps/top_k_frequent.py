2cb265d5efd24d763acad3dcbc8192fc3030060d
ayeryn <erynli@utexas.edu>
2024-06-12 23:33:54 -0500

    update

--- hashmaps/top_k_frequent.py
+++ hashmaps/top_k_frequent.py
@@ -1,6 +1,36 @@
 from collections import defaultdict
 
 
+"""
+A sneaky way to implement bucket sort
+
+Normally, bucket sort initializes an array for each i in nums.
+Here we use freq to store elements that appears i times. Since an element can appear at most len(nums) times so filling out freq takes linear time.
+"""
+
+
+def top_k(nums: list[int], k: int) -> list[int]:
+    freq = [[] for _ in range(len(nums) + 1)]
+    count = defaultdict(int)
+
+    for n in nums:  # O(n)
+        count[n] += 1
+
+    for key in count:  # O(n)
+        c = count[key]
+        freq[c].append(key)
+
+    ans = []
+    # Iterate backwards to get most frequent
+    # if least frequent we can iterate forward
+    for i in range(len(nums), -1, -1):  # O(n)
+        if freq[i] > 0:
+            ans.extend(freq[i])
+
+        if len(ans) == k:
+            return ans
+
+
 def top_k_sorted(nums: list[int], k: int) -> list[int]:
     d = defaultdict(int)
 
