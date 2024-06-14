d615037c3f7116c1155ba7fcfe39dfc8396c3a0e
ayeryn <erynli@utexas.edu>
2024-06-14 17:51:19 -0500

    don't use set for ans

--- arrays/three_sum.py
+++ arrays/three_sum.py
@@ -1,18 +1,24 @@
 def three_sum(nums: list[int]) -> list[list[int]]:
-    ans = set()  # Avoid duplicates
+    ans = []
     nums.sort()
     x = 0
 
     while x < len(nums) and nums[x] <= 0:  # O(n)
         # x has to be negative or 0 to yield a solution
+        if x > 0 and nums[x] == nums[x - 1]:
+            x += 1
+            continue
 
         left, right = x + 1, len(nums) - 1
 
         while left < right:  # O(n)
-            if nums[left] + nums[right] == -nums[i]:
+            if nums[left] + nums[right] == -nums[x]:
                 ans.add((nums[x], nums[left], nums[right]))
-                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
                 right -= 1
+                while nums[right] == nums[right - 1] and left < right:
+                    right -= 1
             elif nums[left] + nums[right] > -nums[x]:
                 right -= 1
             else:
