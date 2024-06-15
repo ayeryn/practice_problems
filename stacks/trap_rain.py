a1977bc3a80e58f9634f3b3c72349897dc23afd4
ayeryn <erynli@utexas.edu>
2024-06-14 21:19:18 -0500

    Complete. prefix max on both sides

--- stacks/trap_rain.py
+++ stacks/trap_rain.py
@@ -1,18 +1,20 @@
 def trap_rain(height: list[int]) -> int:
-    left = 0
     ans = 0
-    while left < len(height):
-        # we can only trap water is there's an "opening" between edges
-        if height[left]:
-            right = left + 1
-            while right < len(height) and not height[right]:
-                right += 1
 
-            if right < len(height):
-                ans += (right - left - 1) * min(height[left], height[right])
+    # Keep track of the max height on both sides excluding itself
+    max_left = [0] * len(height)
+    max_right = [0] * len(height)
 
-            left = right
-        else:
-            left += 1
+    for i in range(1, len(height)):
+        max_left[i] = max(max_left[i - 1], height[i - 1])
+
+    for i in range(len(height) - 2, -1, -1):
+        max_right[i] = max(max_right[i + 1], height[i + 1])
+
+    for i in range(len(height)):
+        # Area is bounded by the smaller edge
+        min_lr = min(max_left[i], max_right[i])
+        if min_lr - height[i] >= 0:
+            ans += min_lr - height[i]
 
     return ans
