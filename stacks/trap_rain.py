491bab28365526de9a90d3306bb378ffef517eb3
ayeryn <erynli@utexas.edu>
2024-06-14 22:15:22 -0500

    add 2pointer implementation

--- stacks/trap_rain.py
+++ stacks/trap_rain.py
@@ -18,3 +18,29 @@ def trap_rain(height: list[int]) -> int:
             ans += min_lr - height[i]
 
     return ans
+
+
+def trap_rain_two_pointer(height):
+    ans = 0
+    l, r = 0, len(height) - 1
+    lm, rm = height[0], height[-1]
+
+    while l < r:
+        # Area is bound by the smaller edge
+        m = min(lm, rm)
+
+        # Update ans only when we update lm or rm
+        if lm < rm:
+            # Calculate water for left when we move forward
+            if m - height[l] >= 0:
+                ans += m - height[l]
+            l += 1
+            lm = max(lm, height[l])
+        else:
+            # Calculate water for right when we move backward
+            if m - height[r] >= 0:
+                ans += m - height[r]
+            r -= 1
+            rm = max(rm, height[r])
+
+    return ans
