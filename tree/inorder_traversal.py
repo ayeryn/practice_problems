5e2649a2e48ef8ee42d06d652f2ae02ca71475fd
ayeryn <erynli@utexas.edu>
2024-07-26 11:58:59 -0500

    optimize

--- tree/inorder_traversal.py
+++ tree/inorder_traversal.py
@@ -12,31 +12,20 @@ def traverse(root: Optional[TreeNode]) -> list[int]:
 
     stack = [root]
     nodes = []
-    seen = set()
 
     while stack:
-        # print(stack[-1].val, [n.val for n in stack], [n.val for n in seen])
-        seen.add(stack[-1])
-
-        if stack[-1].left and stack[-1].left not in seen:  # left
-            seen.add(stack[-1].left)
+        if stack[-1].left:  # left
             stack.append(stack[-1].left)
         else:
-            curr = stack.pop()
-            nodes.append(curr.val)  # node
+            curr = stack.pop()  # node
+            nodes.append(curr.val)
+
+            while stack and not curr.right:
+                # keep popping until we have a node with right child
+                curr = stack.pop()
+                nodes.append(curr.val)
+
             if curr.right:  # right
                 stack.append(curr.right)
-            else:
-                while stack and not stack[-1].right:
-                    nodes.append(stack.pop().val)
 
     return nodes
-
-
-# root = TreeNode(3)
-# left = TreeNode(1)
-# right = TreeNode(2)
-# root.left = left
-# root.right = right
-
-# print(traverse(root))
